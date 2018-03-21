55579f74762dddc883b64af3ec57f364926907a7
Trevor Gevers <tgevers@serverless.solutions.com>
2018-03-20 22:17:20 -0500

    sh project starter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarouselComponent
   ],
   imports: [
-    BrowserModule
+      BrowserModule,
+      NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
